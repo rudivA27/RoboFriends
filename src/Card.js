825ff64ebab969a23f2cc09e7c9aa0c01cd4db44
rudi27 <rvanaarde@gmail.com>
2024-10-15 20:29:43 +0200

    Initial Entry

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+    <div>
+        <img alt='robots' src='' />
+        <div>
+            <h2>Jane Doe</h2>
+            <p>jane.doe@gmail.com</p>
+        </div>
+    </div>
+}
+
+export default Card;
